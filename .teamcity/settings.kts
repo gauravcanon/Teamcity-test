9a8199d318cfbb01f4c319d8fe2ce981204c56b4
Gaurav Dubey <gauravdubey@shiftboolean.com>
2021-02-09 12:30:18 +0530

    build.gradle updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -42,7 +42,7 @@ object BuildTeamcity : BuildType({
     steps {
         gradle {
             tasks = "clean build"
-            gradleWrapperPath = ""
+            gradleWrapperPath = "gradle/wrapper"
         }
     }
 
