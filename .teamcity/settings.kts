5eb95b7218560e92f97bac8cae501bbc4fd039bf
Gaurav Dubey <gauravdubey@shiftboolean.com>
2021-02-09 12:34:23 +0530

    gradle wrapper removed updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -42,7 +42,7 @@ object BuildTeamcity : BuildType({
     steps {
         gradle {
             tasks = "clean build"
-            gradleWrapperPath = "gradle/wrapper"
+            gradleWrapperPath = ""
         }
     }
 
