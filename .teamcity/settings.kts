fbcef56153b3d56dec78ba705f2e9068a5aca53e
Gaurav Dubey <gauravdubey@shiftboolean.com>
2021-02-11 00:01:33 +0530

    readfile using python

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -71,9 +71,9 @@ object BuildTeamcity : BuildType({
         python {
             id = "RUNNER_3"
             name = "Python Test"
-            pythonVersion = customPython {
-                executable = """C:\Users\gaura\anaconda3"""
-            }
+//            pythonVersion = customPython {
+//                executable = """C:\Users\gaura\anaconda3\python"""
+//            }
             command = file {
                 filename = ".teamcity/scripts/version.py"
             }
