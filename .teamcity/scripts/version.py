a53471d98bb5eaf7f89168847fabcac631a2ef79
Gaurav Dubey <gauravdubey@shiftboolean.com>
2021-02-18 23:52:39 +0530

    python file updated

--- .teamcity/scripts/version.py
+++ .teamcity/scripts/version.py
@@ -1,1 +1,18 @@
-print('hello')
+with open(".teamcity/version.txt") as f:
+    file_content = f.read().rstrip("\n")
+    lines = file_content.split("\n")
+
+    values = []
+    for val in lines:
+        words = val.split("=")
+        values.append(words[1])
+
+    version_build = values[0]
+    version_major = values[1]
+    version_minor = values[2]
+    version_patch = values[3]
+
+    print("version_patch= "+version_build)
+    print("version_major= "+version_major)
+    print("version_minor= "+version_minor)
+    print("version_patch= "+version_patch)
