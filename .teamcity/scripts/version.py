2e0981e91f4ba982d1481b236de12e1625d6ab25
Gaurav Dubey <gauravdubey@shiftboolean.com>
2021-02-19 00:57:03 +0530

    fixed script wip ???? In Build Number

--- .teamcity/scripts/version.py
+++ .teamcity/scripts/version.py
@@ -1,23 +1,22 @@
 with open(".teamcity/version.txt") as f:
     file_content = f.read().rstrip("\n")
     lines = file_content.split("\n")
+values = []
+for val in lines:
+    words = val.split("=")
+    values.append(words[1])
 
-    values = []
-    for val in lines:
-        words = val.split("=")
-        values.append(words[1])
+version_build = values[0]
+version_major = values[1]
+version_minor = values[2]
+version_patch = values[3]
 
-    version_build = values[0]
-    version_major = values[1]
-    version_minor = values[2]
-    version_patch = values[3]
+print("version_patch= "+version_build)
+print("version_major= "+version_major)
+print("version_minor= "+version_minor)
+print("version_patch= "+version_patch)
 
-    print("version_patch= "+version_build)
-    print("version_major= "+version_major)
-    print("version_minor= "+version_minor)
-    print("version_patch= "+version_patch)
-
-    counter = '%build.counter%'
-    print(counter)
-    build_number = '##teamcity[buildNumber \'{}.{}.{}.{}\']'.format(counter,version_major,version_minor,version_patch)
-    print(build_number)
+counter = '%build.counter%'
+print(counter)
+build_number = '##teamcity[buildNumber \'{}.{}.{}.{}\']'.format(counter,version_major,version_minor,version_patch)
+print(build_number)
